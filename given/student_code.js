865eaff447190396a71373b02625e9653936b81f
Joshua Diaddigo <joshuad95@hotmail.com>
2016-03-16 01:33:35 -0400

    Initial commit

---
+++ given/student_code.js
@@ -0,0 +1,36 @@
+kiwi_caught = 0;
+
+function process_frame() {
+	// your code here
+}
+
+function translate_truck_right() {
+	// your code here
+}
+
+function translate_truck_left() {
+	// your code here
+}
+
+function coast_truck() {
+	// your code here
+}
+
+function brake_truck() {
+	// your code here
+}
+
+function compute_truck_position() {
+	// your code here
+}
+
+function find_collisions(kiwi) {
+	// your code here
+}
+
+function end_game() {
+	// your code here
+}
+
+// just ignore this (but don't delete it):
+check_latest = true;
